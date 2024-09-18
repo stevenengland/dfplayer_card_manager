496da3d80fe665f753f8b2323fc44240cf7c6d07
StEn <github@steven-england.info>
2024-09-18 15:21:07 +0200

    test: add fake file system support

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,25 @@
+from pathlib import PurePath
+
 import pytest
 from mockito import unstub
+
+from tests.file_system_helper import FakeFileSystemHelper
+
+BASE_DIR = PurePath(__file__).parent.parent
+TEST_ASSETS_DIR = BASE_DIR.joinpath("tests", "test_assets")
 
 
 @pytest.fixture
 def unstub_all():
     yield
     unstub()
+
+
+@pytest.fixture
+def test_assets_fs(fs):
+    fs.add_real_directory(TEST_ASSETS_DIR)
+    fsh = FakeFileSystemHelper(TEST_ASSETS_DIR, fs)
+    yield fsh
 
 
 def pytest_addoption(parser):
