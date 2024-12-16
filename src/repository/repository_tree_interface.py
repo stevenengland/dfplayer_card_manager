feb1d52220fa246897f07819d8621cced7756a71
StEn <github@steven-england.info>
2024-12-16 10:59:32 +0100

    refactor: update method signatures to include default parameters for valid directory and file patterns

--- src/repository/repository_tree_interface.py
+++ src/repository/repository_tree_interface.py
@@ -5,15 +5,19 @@ from src.repository.repository_element import RepositoryElement
 
 class RepositoryTreeInterface(ABC):
     @abstractmethod
-    def get_target_tree(self, sd_root_path: str) -> list[str]:
+    def get_valid_root_dirs(
+        self,
+        root_dir: str,
+        valid_dir_pattern: str = "",
+    ) -> list[str]:
         pass
 
     @abstractmethod
-    def get_valid_root_dirs(self, root_dir: str, valid_dir_pattern: str) -> list[str]:
-        pass
-
-    @abstractmethod
-    def get_valid_subdir_files(self, subdir: str, valid_file_pattern: str) -> list[str]:
+    def get_valid_subdir_files(
+        self,
+        subdir: str,
+        valid_file_pattern: str = "",
+    ) -> list[str]:
         pass
 
     @abstractmethod
--- src/repository/target_repository_tree.py
+++ src/repository/target_repository_tree.py
@@ -7,10 +7,11 @@ from src.repository.repository_tree_interface import RepositoryTreeInterface
 
 class TargetRepositoryTree(RepositoryTreeInterface):
 
-    def get_target_tree(self, root_dir: str) -> list[str]:
-        return os.listdir(root_dir)  # Fake implementation
-
-    def get_valid_root_dirs(self, root_dir: str, valid_dir_pattern: str) -> list[str]:
+    def get_valid_root_dirs(
+        self,
+        root_dir: str,
+        valid_dir_pattern: str = "",
+    ) -> list[str]:
         subdirs = [
             entry
             for entry in os.listdir(root_dir)
@@ -20,7 +21,11 @@ class TargetRepositoryTree(RepositoryTreeInterface):
         subdirs.sort()
         return subdirs
 
-    def get_valid_subdir_files(self, subdir: str, valid_file_pattern: str) -> list[str]:
+    def get_valid_subdir_files(
+        self,
+        subdir: str,
+        valid_file_pattern: str = "",
+    ) -> list[str]:
         valid_files = [
             entry
             for entry in os.listdir(subdir)
