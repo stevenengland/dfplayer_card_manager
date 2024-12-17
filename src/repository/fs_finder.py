6561d4b08050add31b991c1e8d9bb467d3b8eb62
StEn <github@steven-england.info>
2024-12-17 09:37:59 +0100

    feat: implement get_repository_tree function for building repository structure with optional file pattern overrides

--- src/repository/fs_finder.py
+++ src/repository/fs_finder.py
@@ -1,5 +1,6 @@
 import os
 import re
+from typing import Optional
 
 
 def get_valid_root_dirs(
@@ -28,3 +29,29 @@ def get_valid_subdir_files(
     ]
     valid_files.sort()
     return valid_files
+
+
+def get_repository_tree(
+    root_dir: str,
+    valid_root_subdir_pattern: str,
+    valid_subdir_files_pattern: str,
+    valid_subdir_files_pattern_overrides: Optional[dict[str, str]] = None,
+) -> list[tuple[str, str]]:
+    if valid_subdir_files_pattern_overrides is None:
+        valid_subdir_files_pattern_overrides = {}
+    repository_tree = []
+    # get valid root dirs
+    valid_root_subdirs = get_valid_root_dirs(root_dir, valid_root_subdir_pattern)
+    for valid_root_subdir in valid_root_subdirs:
+        # get valid files in subdir
+        valid_subdir = os.path.join(root_dir, valid_root_subdir)
+        # if the valid subdir is listed in the overrides, use the override pattern
+        if valid_root_subdir in valid_subdir_files_pattern_overrides:
+            valid_subdir_files_pattern = valid_subdir_files_pattern_overrides.get(
+                valid_root_subdir,
+                valid_subdir_files_pattern,
+            )
+        valid_files = get_valid_subdir_files(valid_subdir, valid_subdir_files_pattern)
+        for valid_file in valid_files:
+            repository_tree.append((valid_root_subdir, valid_file))
+    return repository_tree
--- tests/test_fs_finder.py
+++ tests/test_fs_finder.py
@@ -2,7 +2,11 @@ import os
 
 import pytest
 
-from src.repository.fs_finder import get_valid_root_dirs, get_valid_subdir_files
+from src.repository.fs_finder import (
+    get_repository_tree,
+    get_valid_root_dirs,
+    get_valid_subdir_files,
+)
 
 pytestmark = pytest.mark.usefixtures("unstub")
 e2e = pytest.mark.skipif("not config.getoption('e2e')")
@@ -69,3 +73,96 @@ class TestValidFileDetectionInSubdirs:
             "001.mp3",
             "006.mp3",
         ]
+
+
+class TestRepositoryTreeCreation:
+    def test_get_repository_tree_creates_correct_tree_wo_overrides(
+        self,
+        when,
+    ):
+        # GIVEN
+        root_dir = "/root"
+        when(os).listdir(root_dir).thenReturn(
+            [
+                "01",
+                "02",
+                "03",
+            ],
+        )
+        when(os).listdir(os.path.join(root_dir, "01")).thenReturn(
+            [
+                "001.mp3",
+                "002.mp3",
+            ],
+        )
+        when(os).listdir(os.path.join(root_dir, "02")).thenReturn(
+            [],  # empty dir
+        )
+        when(os).listdir(os.path.join(root_dir, "03")).thenReturn(
+            [
+                "001.mp3",
+                "002.mp3",
+                "003.mp3",
+            ],  # extra file
+        )
+        when(os.path).isdir(...).thenReturn(True)
+        when(os.path).isfile(...).thenReturn(True)
+
+        # WHEN
+        repository_tree = get_repository_tree(
+            root_dir,
+            r"^\d{2}$",
+            r"\d{3}\.mp3",
+        )
+        # THEN
+        assert len(repository_tree) == 5
+        assert repository_tree[0] == ("01", "001.mp3")
+        assert repository_tree[1] == ("01", "002.mp3")
+        assert repository_tree[2] == ("03", "001.mp3")
+        assert repository_tree[3] == ("03", "002.mp3")
+        assert repository_tree[4] == ("03", "003.mp3")
+
+    def test_get_repository_tree_creates_correct_tree_with_overrides(
+        self,
+        when,
+    ):
+        # GIVEN
+        root_dir = "/root"
+        when(os).listdir(root_dir).thenReturn(
+            [
+                "01",
+                "02",
+                "03",
+            ],
+        )
+        when(os).listdir(os.path.join(root_dir, "01")).thenReturn(
+            [
+                "001.mp3",
+                "002.mp3",
+            ],
+        )
+        when(os).listdir(os.path.join(root_dir, "02")).thenReturn(
+            [],  # empty dir
+        )
+        when(os).listdir(os.path.join(root_dir, "03")).thenReturn(
+            [
+                "001.mp3",
+                "02.mp3",
+                "003.mp3",
+            ],  # extra file
+        )
+        when(os.path).isdir(...).thenReturn(True)
+        when(os.path).isfile(...).thenReturn(True)
+
+        # WHEN
+        repository_tree = get_repository_tree(
+            root_dir,
+            r"^\d{2}$",
+            r"\d{3}\.mp3",
+            {"03": r"^\d{2}\.mp3$"},
+        )
+        # THEN
+        assert len(repository_tree) == 3
+        assert repository_tree[0] == ("01", "001.mp3")
+        assert repository_tree[1] == ("01", "002.mp3")
+        assert repository_tree[2] == ("03", "02.mp3")
