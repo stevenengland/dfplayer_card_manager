50bd4c938a17ff4e280df2661df42ee36318453f
StEn <github@steven-england.info>
2024-12-19 16:59:27 +0100

    feat: add abstract method read_audio_content_and_id3_tags to AudioFileManagerInterface

--- src/mp3/audio_file_manager_interface.py
+++ src/mp3/audio_file_manager_interface.py
@@ -1,9 +1,17 @@
 from abc import ABC, abstractmethod
+from typing import Tuple
 
 from src.mp3.tag_collection import TagCollection
 
 
 class AudioFileManagerInterface(ABC):
+    @abstractmethod
+    def read_audio_content_and_id3_tags(
+        self,
+        file_path: str,
+    ) -> Tuple[bytes, TagCollection]:
+        pass
+
     @abstractmethod
     def read_id3_tags(self, file_path: str) -> TagCollection:
         pass
