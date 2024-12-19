1c1c64fe318e136c428e08baff181603cc7f1528
StEn <github@steven-england.info>
2024-12-19 17:00:55 +0100

    feat: add repo_root_dir and hash attributes to RepositoryElement

--- src/repository/repository_element.py
+++ src/repository/repository_element.py
@@ -5,9 +5,11 @@ from dataclasses import dataclass
 class RepositoryElement:  # noqa: WPS230
     def __init__(self) -> None:
         self.tree_id = ""
+        self.repo_root_dir = ""
         self.dir = ""
         self.file_name = ""
         self.title = ""
         self.artist = ""
         self.album = ""
         self.track_number = 0
+        self.hash = ""
