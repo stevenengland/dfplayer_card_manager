91efaf666f623eeea3fce238785e3863083bbe1a
StEn <github@steven-england.info>
2024-12-20 21:05:57 +0100

    feat: add methods to update element title, album, and artist based on directory patterns

--- src/repository/repository_element_updater.py
+++ src/repository/repository_element_updater.py
@@ -1,7 +1,6 @@
 import re
 
 from src.config.configuration import RepositoryConfig
-from src.repository.detection_source import DetectionSource
 from src.repository.repository_element import RepositoryElement
 
 
@@ -9,23 +8,57 @@ def update_element_by_dir(
     element: RepositoryElement,
     effective_config: RepositoryConfig,
 ) -> None:
-    update_element_title_by_dir(element, effective_config)
+    if not effective_config.valid_subdir_pattern:
+        raise ValueError("No valid subdir pattern set")
+    if not effective_config.title_match or effective_config.title_match < 1:
+        raise ValueError("No valid album match set")
+    update_element_title_by_dir(
+        element,
+        effective_config.valid_subdir_pattern,
+        effective_config.title_match,
+    )
+
+
+def update_element_album_by_dir(
+    element: RepositoryElement,
+    dir_pattern: str,
+    album_match: int,
+) -> None:
+    dir_match_result = _get_dir_match(element, dir_pattern, album_match)
+
+    element.album = dir_match_result
+
+
+def update_element_artist_by_dir(
+    element: RepositoryElement,
+    dir_pattern: str,
+    artist_match: int,
+) -> None:
+
+    dir_match_result = _get_dir_match(element, dir_pattern, artist_match)
+
+    element.artist = dir_match_result
 
 
 def update_element_title_by_dir(
     element: RepositoryElement,
-    effective_config: RepositoryConfig,
+    dir_pattern: str,
+    title_match: int,
 ) -> None:
-    if not effective_config.valid_subdir_pattern:
-        raise ValueError("No valid subdir pattern set")
-    if not effective_config.title_match or effective_config.title_match < 1:
-        raise ValueError("No valid title match set")
-    if effective_config.title_source == DetectionSource.dirname:
-        title_matched_text = re.search(
-            effective_config.valid_subdir_pattern,
-            element.dir,
-        )
-        if title_matched_text:
-            if len(title_matched_text.groups()) < effective_config.title_match:
-                return
-            element.title = title_matched_text.group(effective_config.title_match)
+    dir_match_result = _get_dir_match(element, dir_pattern, title_match)
+
+    element.title = dir_match_result
+
+
+def _get_dir_match(element: RepositoryElement, dir_pattern: str, field_match: int):
+    if field_match < 1:
+        raise ValueError("Match number must be 1 or greater")
+    field_matched_text = re.search(
+        dir_pattern,
+        element.dir,
+    )
+    if field_matched_text:
+        if len(field_matched_text.groups()) < field_match:
+            return None
+        artist_result = field_matched_text.group(field_match)
+    return artist_result
--- tests/test_repository_element_updater.py
+++ tests/test_repository_element_updater.py
@@ -1,71 +1,127 @@
 import pytest
 
-from src.config.configuration import RepositoryConfig
 from src.repository import repository_element_updater
-from src.repository.detection_source import DetectionSource
 from src.repository.repository_element import RepositoryElement
-from tests.factories.configuration_factory import create_source_repo_config
 
 pytestmark = pytest.mark.usefixtures("unstub")
 e2e = pytest.mark.skipif("not config.getoption('e2e')")
 
 
-class TestElementUpdates:
+class TestElementTitleUpdates:
     def test_title_gets_updated(self, when):
         # GIVEN
-        config = RepositoryConfig()
-        config.valid_subdir_pattern = r"^\d{2}\.(no)\.(yes).*$"
-        config.title_source = DetectionSource.dirname
-        config.title_match = 2
         element = RepositoryElement()
         element.dir = "01.no.yes.loremipsum"
         # WHEN
-        repository_element_updater.update_element_title_by_dir(element, config)
+        repository_element_updater.update_element_title_by_dir(
+            element,
+            r"^\d{2}\.(no)\.(yes).*$",
+            2,
+        )
         # THEN
         assert element.title == "yes"
 
-    def test_title_gets_updated_raises_when_title_match_is_0(self):
+    def test_title_gets_updated_raises_when_title_match_lt_1(self):
         # GIVEN
-        config = create_source_repo_config()
-        config.title_source = DetectionSource.dirname
-        config.title_match = 0
         element = RepositoryElement()
         # WHEN
         # THEN
-        with pytest.raises(ValueError, match="match"):
-            repository_element_updater.update_element_title_by_dir(element, config)
-
-    def test_title_gets_updated_raises_when_title_match_is_none(self):
-        # GIVEN
-        config = create_source_repo_config()
-        config.title_source = DetectionSource.dirname
-        config.title_match = None
-        element = RepositoryElement()
-        # WHEN
-        # THEN
-        with pytest.raises(ValueError, match="match"):
-            repository_element_updater.update_element_title_by_dir(element, config)
-
-    def test_title_gets_updated_raises_when_valid_subdir_pattern_is_none(self):
-        # GIVEN
-        config = create_source_repo_config()
-        config.valid_subdir_pattern = None
-        element = RepositoryElement()
-        # WHEN
-        # THEN
-        with pytest.raises(ValueError, match="pattern"):
-            repository_element_updater.update_element_title_by_dir(element, config)
+        with pytest.raises(ValueError, match="Match"):
+            repository_element_updater.update_element_title_by_dir(
+                element,
+                r"^\d{2}\.(no)\.(yes).*$",
+                0,
+            )
 
     def test_title_isnt_updated_when_title_match_is_too_large(self):
         # GIVEN
-        config = RepositoryConfig()
-        config.valid_subdir_pattern = r"^\d{2}\.(no)\.(yes).*$"
-        config.title_source = DetectionSource.dirname
-        config.title_match = 99
         element = RepositoryElement()
-        before = element.title
         element.dir = "01.no.yes.loremipsum"
         # WHEN
-        repository_element_updater.update_element_title_by_dir(element, config)
+        repository_element_updater.update_element_title_by_dir(
+            element,
+            r"^\d{2}\.(no)\.(yes).*$",
+            99,
+        )
         # THEN
-        assert element.title == before
+        assert element.title is None
+
+
+class TestElementAlbumUpdates:
+    def test_album_gets_updated(self, when):
+        # GIVEN
+        element = RepositoryElement()
+        element.dir = "01.no.yes.loremipsum"
+        # WHEN
+        repository_element_updater.update_element_album_by_dir(
+            element,
+            r"^\d{2}\.(no)\.(yes).*$",
+            1,
+        )
+        # THEN
+        assert element.album == "no"
+
+    def test_album_gets_updated_raises_when_album_match_lt_1(self):
+        # GIVEN
+        element = RepositoryElement()
+        # WHEN
+        # THEN
+        with pytest.raises(ValueError, match="Match"):
+            repository_element_updater.update_element_album_by_dir(
+                element,
+                r"^\d{2}\.(no)\.(yes).*$",
+                0,
+            )
+
+    def test_album_isnt_updated_when_album_match_is_too_large(self):
+        # GIVEN
+        element = RepositoryElement()
+        element.dir = "01.no.yes.loremipsum"
+        # WHEN
+        repository_element_updater.update_element_album_by_dir(
+            element,
+            r"^\d{2}\.(no)\.(yes).*$",
+            99,
+        )
+        # THEN
+        assert element.album is None
+
+
+class TestElementArtistUpdates:
+    def test_artist_gets_updated(self, when):
+        # GIVEN
+        element = RepositoryElement()
+        element.dir = "01.no.yes.loremipsum"
+        # WHEN
+        repository_element_updater.update_element_artist_by_dir(
+            element,
+            r"^\d{2}\.(no)\.(yes).*$",
+            1,
+        )
+        # THEN
+        assert element.artist == "no"
+
+    def test_artist_gets_updated_raises_when_artist_match_lt_1(self):
+        # GIVEN
+        element = RepositoryElement()
+        # WHEN
+        # THEN
+        with pytest.raises(ValueError, match="Match"):
+            repository_element_updater.update_element_artist_by_dir(
+                element,
+                r"^\d{2}\.(no)\.(yes).*$",
+                0,
+            )
+
+    def test_artist_isnt_updated_when_artist_match_is_too_large(self):
+        # GIVEN
+        element = RepositoryElement()
+        element.dir = "01.no.yes.loremipsum"
+        # WHEN
+        repository_element_updater.update_element_artist_by_dir(
+            element,
+            r"^\d{2}\.(no)\.(yes).*$",
+            99,
+        )
+        # THEN
+        assert element.artist is None
