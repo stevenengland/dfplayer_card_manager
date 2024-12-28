b07e3b46a686767edffa0ced7b99ccfea1166f77
StEn <github@steven-england.info>
2024-12-28 10:02:09 +0100

    test: add real directory to FakeFileSystemHelper for improved test asset management

--- tests/file_system_helper.py
+++ tests/file_system_helper.py
@@ -7,6 +7,7 @@ class FakeFileSystemHelper:
     def __init__(self, test_assets_path: PurePath, file_system: FakeFilesystem) -> None:
         self._test_assets_path = test_assets_path
         self._file_system = file_system
+        self._file_system.add_real_directory(str(test_assets_path))
 
     @property
     def test_assets_path(self) -> PurePath:
