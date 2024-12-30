5b53bc123928d47542de6509b5a9470ed142162f
StEn <github@steven-england.info>
2024-12-30 17:09:05 +0100

    feat: enhance FAT32 check with allocation unit size validation and improved error handling

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,9 +4,18 @@ from typer.testing import CliRunner
 from dfplayer_card_manager.cli.cli import app
 from dfplayer_card_manager.fat import fat_checker
 
+pytestmark = pytest.mark.usefixtures("unstub")
+e2e = pytest.mark.skipif("not config.getoption('e2e')")
+
 
 @pytest.fixture(scope="function", name="cli_runner")
-def get_cli_runner() -> CliRunner:
+def get_cli_runner(monkeypatch) -> CliRunner:
+    monkeypatch.setattr(fat_checker, "check_is_fat32", lambda _filesystem_path: True)
+    monkeypatch.setattr(
+        fat_checker,
+        "check_has_correct_allocation_unit_size",
+        lambda _filesystem_path: True,
+    )
     return CliRunner()
 
 
@@ -22,4 +31,46 @@ class TestChecks:
         fat32_check_output = cli_runner.invoke(app, ["check", "tests/test_assets"])
         # THEN
         assert fat32_check_output.exit_code != 0
-        assert "fat32" in fat32_check_output.stdout
+        assert "is not a path within a FAT32" in fat32_check_output.stdout
+
+    def test_fat_check_returns_true(
+        self,
+        cli_runner,
+        when,
+    ):
+        # GIVEN
+        when(fat_checker).check_is_fat32(...).thenReturn(True)
+        # WHEN
+        fat32_check_output = cli_runner.invoke(app, ["check", "tests/test_assets"])
+        # THEN
+        assert fat32_check_output.exit_code == 0
+        assert "is a path within a FAT32" in fat32_check_output.stdout
+
+    def test_fat_check_returns_false_allocation_unit_size(
+        self,
+        cli_runner,
+        when,
+    ):
+        # GIVEN
+        when(fat_checker).check_has_correct_allocation_unit_size(...).thenReturn(False)
+        # WHEN
+        fat32_check_output = cli_runner.invoke(app, ["check", "tests/test_assets"])
+        # THEN
+        assert fat32_check_output.exit_code == 0  # Since the check is not fatal
+        assert (
+            "does not have the correct allocation unit size"
+            in fat32_check_output.stdout
+        )
+
+    def test_fat_check_returns_true_allocation_unit_size(
+        self,
+        cli_runner,
+        when,
+    ):
+        # GIVEN
+        when(fat_checker).check_has_correct_allocation_unit_size(...).thenReturn(True)
+        # WHEN
+        fat32_check_output = cli_runner.invoke(app, ["check", "tests/test_assets"])
+        # THEN
+        assert fat32_check_output.exit_code == 0
+        assert "has the correct allocation unit size" in fat32_check_output.stdout
