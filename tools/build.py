e43b415b411e55d7abc56ce5d4b3c5cf9c721dbc
StEn <github@steven-england.info>
2025-01-11 16:39:24 +0100

    feat: add build script with checks for package distribution

---
+++ tools/build.py
@@ -0,0 +1,48 @@
+import os
+import subprocess  # noqa: S404
+import sys
+
+
+def get_os_specific_command_directory() -> str:
+    return r".venv\Scripts" if os.name == "nt" else ".venv/bin"
+
+
+def call_build(argv: list[str]) -> None:
+    print("** BUILD **")
+    cmd_path = os.path.join(get_os_specific_command_directory(), "python")
+    cmd = [cmd_path, "setup.py", "sdist", "bdist_wheel"]
+    if argv:
+        cmd.extend(argv)
+    try:
+        subprocess.run(cmd, check=True)  # noqa: S607, S603
+    except FileNotFoundError:
+        print("Module setup.py not found. Please make sure it is installed.")
+        exit(1)
+
+    print("Calling build completed successfully.")
+
+
+def call_build_check_twine() -> None:
+    print("** BUILD CHECK TWINE **")
+    cmd_path = os.path.join(get_os_specific_command_directory(), "python")
+    cmd = [cmd_path, "-m", "twine", "check", "--strict", f"dist{os.sep}*"]
+    subprocess.run(cmd, check=True)  # noqa: S607, S603
+
+    print("Calling twine check completed successfully.")
+
+
+def call_build_check_wheel() -> None:
+    print("** BUILD CHECK WHEEL **")
+    cmd_path = os.path.join(get_os_specific_command_directory(), "check-wheel-contents")
+    cmd = [
+        cmd_path,
+        f"dist{os.sep}*.whl",
+    ]
+    subprocess.run(cmd, check=True)  # noqa: S607, S603
+
+    print("Calling twine check completed successfully.")
+
+
+call_build(sys.argv[1:])
+call_build_check_twine()
+call_build_check_wheel()
