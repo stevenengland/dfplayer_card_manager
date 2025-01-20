ad5d71e8adefba4da1676ef5b36c732807e2df16
StEn <github@steven-england.info>
2025-01-20 20:36:03 +0100

    refactor: enhance read_override_config for better resilience and structure

--- src/dfplayer_card_manager/config/config_reader.py
+++ src/dfplayer_card_manager/config/config_reader.py
@@ -1,14 +1,27 @@
 import os
 
 from dfplayer_card_manager.config import config_checker, yaml_config
-from dfplayer_card_manager.config.configuration import OverrideConfig
+from dfplayer_card_manager.config.configuration import (
+    OverrideConfig,
+    ProcessingConfig,
+    RepositoryConfig,
+)
 
 
 def read_override_config(config_file_path: str) -> OverrideConfig:
     if not os.path.isfile(config_file_path):
         raise FileNotFoundError("Configuration file not found")
 
-    config = yaml_config.create_yaml_object(config_file_path, OverrideConfig)
-    config_checker.check_override_config(config)
+    read_config: OverrideConfig = yaml_config.create_yaml_object(
+        config_file_path,
+        OverrideConfig,
+    )
 
-    return config
+    return_config = OverrideConfig(
+        repository_source=read_config.repository_source or RepositoryConfig(),
+        repository_processing=read_config.repository_processing or ProcessingConfig(),
+    )
+
+    config_checker.check_override_config(return_config)
+
+    return return_config
